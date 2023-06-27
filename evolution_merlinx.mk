a533952f4156690e1224103b769e1b0fa98d79d0
crackedmodmenu <121314959+crackedmodmenu@users.noreply.github.com>
2023-06-27 14:52:00 +0200

    Update and rename lineage_merlinx.mk to evolution_merlinx.mk

--- evolution_merlinx.mk
+++ evolution_merlinx.mk
@@ -11,10 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/merlinx/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Evolution-X stuff. 
+ $(call inherit-product, vendor/evolution/config/common_full_phone.mk) 
+ EVO_BUILD_TYPE := UNOFFICIAL
 
-PRODUCT_NAME := lineage_merlinx
+PRODUCT_NAME := evolution_merlinx
 PRODUCT_DEVICE := merlinx
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
