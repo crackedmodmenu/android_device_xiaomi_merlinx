1114f75f18363a21069d6513217be9f88f6b206a
crackedmodmenu <121314959+crackedmodmenu@users.noreply.github.com>
2023-07-30 18:17:49 +0200

    CherishOS bringup

--- device.mk
+++ device.mk
@@ -15,6 +15,14 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 2340
 
+# Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.device@3.6.vendor:64 \
+    android.hardware.camera.provider@2.6.vendor:64
+
+PRODUCT_PACKAGES += \
+    Camera
+
 # Firmware
 RECOVERY_TS_FW_PATH := vendor/xiaomi/merlinx/proprietary/vendor/firmware
 
